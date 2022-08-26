0df994946702705765bc303de9f8d2ab7d845457
fanxiaohao <fanxiaohao@mail.taiji.com.cn>
2022-08-26 17:23:30 +0800

    完成井字棋

--- src/index.js
+++ src/index.js
@@ -2,29 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  // 在 JavaScript class 中，每次你定义其子类的构造函数时，都需要调用 super 方法。
-  // 因此，在所有含有构造函数的的 React 组件中，构造函数必须以 super(props) 开头。
-  constructor(props) {
-    super(props);
-    // 数据仓库
-    this.state = {
-      value: null
-    };
-  }
+// class Square extends React.Component {
+// 在 JavaScript class 中，每次你定义其子类的构造函数时，都需要调用 super 方法。
+// 因此，在所有含有构造函数的的 React 组件中，构造函数必须以 super(props) 开头。
+// constructor(props) {
+//   super(props);
+//   // 数据仓库
+//   this.state = {
+//     value: null
+//   };
+// }
 
-  render() {
-    return (
-        <button className="square"
-            onClick={() => {
-              // 每次在组件中调用 setState 时，React 都会自动更新其子组件。
-              this.setState({value: 'X'});
-            }}
-        >
-          {this.state.value}
-        </button>
-    );
-  }
+//   render() {
+//     return (
+//         <button
+//             className="square"
+//             onClick={() => {
+//               // 每次在组件中调用 setState 时，React 都会自动更新其子组件。
+//               this.props.onClick();
+//             }}
+//         >
+//           {this.props.value}
+//         </button>
+//     );
+//   }
+// }
+
+
+//函数组件
+function Square(props) {
+  return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+  )
 }
 
 class Board extends React.Component {
@@ -32,15 +43,40 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    // 该函数不做任何处理直接返回。
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares, xIsNext: !this.state.xIsNext
+    });
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    // 在return 后加 () 不会破坏代码结构
+    return (
+        // 通常会将代表事件的监听 prop 命名为 on[Event]，将处理事件的监听方法命名为 handle[Event] 这样的格式。
+        <Square value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? "X" : 'O');
+    }
 
     return (
         <div>
@@ -85,3 +121,24 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game/>);
+
+// 判断输赢
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
